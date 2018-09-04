d051071eef70d938f14a8c981fe9ad0b3bae1be6
YanC Lin <yonalin1@gmail.com>
2018-09-04 18:59:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ function printBadges(name){
 for (let i = 0; i < names.length; i++) {
   console.log('Welcome ${names[i]}! You are employee #${i+1}.')
 }
-  return names 
+  return names
+}
+
+function tailsNeverFails() {
+  let tails = 0 
+
+  while (Math.random() >= .5){
+    tails++
+  }
+  return 'You got ${tails} tails in a row!'
 }
