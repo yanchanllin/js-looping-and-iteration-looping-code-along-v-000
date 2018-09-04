775024dc81884f015a0236ed7094e7d8ed0f4dd7
YanC Lin <yonalin1@gmail.com>
2018-09-04 19:00:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-// Code your solutions in this file
-function printBadges(name){
-for (let i = 0; i < names.length; i++) {
-  console.log('Welcome ${names[i]}! You are employee #${i+1}.')
-}
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i+1}.`)
+  }
   return names
 }
 
 function tailsNeverFails() {
-  let tails = 0 
+  let tails = 0
 
-  while (Math.random() >= .5){
+  while (Math.random() >= .5) {
     tails++
   }
-  return 'You got ${tails} tails in a row!'
+  return `You got ${tails} tails in a row!`
 }
